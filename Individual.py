dc2a20d013f13eae3fd447de85c3452429bf1d13
ItsMyLife1337 <itsmylife1337@yandex.ru>
2022-10-01 08:10:53 +0300

    Исправления кода

--- Individual.py
+++ Individual.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     print("Список команд:\n")
     print("add - добавить студента;")
     print("list - вывести список студентов;")
-    print("select <средний балл> - запросить студентов с баллом выше 4.0;")
+    print("select - запросить студентов с баллом выше 4.0;")
     print("exit - завершить работу с программой.")
 
     # Организовать бесконечный цикл запроса команд.
@@ -25,12 +25,18 @@ if __name__ == '__main__':
             # Запросить данные о студенте.
             name = input("Фамилия и инициалы? ")
             group = input("Номер группы? ")
-            mark = str(input('Успеваемость: '))
+            progress = [
+                int(input("Оценка за 1 семестр")),
+                int(input("Оценка за 2 семестр")),
+                int(input("Оценка за 3 семестр")),
+                int(input("Оценка за 4 семестр")),
+                int(input("Оценка за 5 семестр"))
+            ]
             # Создать словарь.
             student = {
                 'name': name,
                 'group': group,
-                'mark': mark,
+                'mark': progress
             }
             # Добавить словарь в список.
             students.append(student)
@@ -58,21 +64,27 @@ if __name__ == '__main__':
 
             # Вывести данные о всех студентах.
             for idx, student in enumerate(students, 1):
+                ma = student.get('mark', '')
                 print(
-                    '| {:>4} | {:<30} | {:<20} | {:>15} |'.format(
+                    '| {:^4} | {:<30} | {:<20} | {}.{}.{}.{}.{:<7} |'.format(
                         idx,
                         student.get('name', ''),
                         student.get('group', ''),
-                        student.get('mark', 0)
+                        ma[0],
+                        ma[1],
+                        ma[2],
+                        ma[3],
+                        ma[4]
                     )
                 )
             print(line)
+
         elif command.startswith('select'):
             # Инициализировать счетчик.
             count = 0
             # Проверить сведения студентов из списка.
             for student in students:
-                mark = list(map(int, student.get('mark', '').split()))
+                mark = student.get('mark', '')
                 if sum(mark) / max(len(mark), 1) >= 4.0:
                     print(
                         '{:>4} {}'.format('*', student.get('name', '')),
@@ -83,4 +95,3 @@ if __name__ == '__main__':
                 print("Студенты с баллом 4.0 и выше не найдены.")
         else:
             print(f"Неизвестная команда {command}", file=sys.stderr)
-
